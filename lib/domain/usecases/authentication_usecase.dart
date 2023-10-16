c1564771913668e93064c808b2adf2eb1b56703f
Thierry Oliveira <thierry.oliveira@superformula.com>
2023-10-16 15:24:57 -0300

    refactor: change auth params to a class instead of named ones

--- lib/domain/usecases/authentication_usecase.dart
+++ lib/domain/usecases/authentication_usecase.dart
@@ -1,5 +1,15 @@
 import '../entities/entities.dart';
 
 abstract class AuthenticationUseCase {
-  Future<AccountEntity> auth({required String email, required String password});
+  Future<AccountEntity> auth({required AuthenticationParams params});
+}
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({
+    required this.email,
+    required this.password,
+  });
 }
