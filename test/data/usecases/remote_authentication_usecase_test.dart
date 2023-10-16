31ad865725c1f3e8d5cae37f6a76e2f3427177cc
Thierry Oliveira <thierry.oliveira@superformula.com>
2023-10-16 15:57:55 -0300

    feat: ensure remote authentication throws an UnexpectedError on HttpError.serverError

--- test/data/usecases/remote_authentication_usecase_test.dart
+++ test/data/usecases/remote_authentication_usecase_test.dart
@@ -60,4 +60,16 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('should throws UnexpectedError if Httpclient returns 500', () async {
+    when(() => httpClient.request(
+          url: any(named: 'url'),
+          method: any(named: 'method'),
+          body: any(named: 'body'),
+        )).thenThrow(HttpError.serverError);
+
+    final future = sut.auth(params: params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
