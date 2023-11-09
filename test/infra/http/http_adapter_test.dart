da9d812cb307b1a11e01955aeca19ba3b71101cb
Thierry Oliveira <thierry.oliveira@superformula.com>
2023-11-09 12:41:14 -0300

    test: ensures http adapter calls post with/out body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -16,13 +18,16 @@ class HttpAdapter {
     required String url,
     required HttpMethods method,
     Map<String, dynamic>? headers,
+    Map<String, dynamic>? body,
   }) async {
     final defaultHeaders = {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
     final uri = Uri.parse(url);
-    await client.post(uri, headers: {...defaultHeaders, ...?headers});
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(uri,
+        headers: {...defaultHeaders, ...?headers}, body: jsonBody);
   }
 }
 
@@ -45,43 +50,59 @@ void main() {
     sut = HttpAdapter(client: client);
   });
 
-  test('should call post with correct values', () async {
-    when(() => client.post(any()))
-        .thenAnswer((invocation) async => Response('anything', 200));
+  group('post', () {
+    test('should call post with correct values', () async {
+      when(() => client.post(any(),
+              headers: any(named: 'headers'), body: any(named: 'body')))
+          .thenAnswer((invocation) async => Response('anything', 200));
 
-    await sut.request(url: url, method: HttpMethods.post);
+      await sut.request(url: url, method: HttpMethods.post, body: {
+        'any_key': 'any_value',
+      });
 
-    verify(() => client.post(uri));
-  });
+      verify(() => client.post(
+            uri,
+            headers: {
+              'content-type': 'application/json',
+              'accept': 'application/json',
+            },
+            body: '{"any_key":"any_value"}',
+          ));
+    });
 
-  test('should call post with correct values', () async {
-    when(() => client.post(any()))
-        .thenAnswer((invocation) async => Response('anything', 200));
+    test('should call post properly without body', () async {
+      when(() => client.post(any(),
+              headers: any(named: 'headers'), body: any(named: 'body')))
+          .thenAnswer((invocation) async => Response('anything', 200));
 
-    await sut.request(url: url, method: HttpMethods.post);
+      await sut.request(url: url, method: HttpMethods.post);
 
-    verify(() => client.post(uri));
-  });
+      verify(() => client.post(
+            any(),
+            headers: any(named: 'headers'),
+          ));
+    });
 
-  test('should call post with correct headers', () async {
-    when(() => client.post(any(), headers: any(named: 'headers')))
-        .thenAnswer((invocation) async => Response('anything', 200));
+    test('should call post with correct custom headers', () async {
+      when(() => client.post(any(), headers: any(named: 'headers')))
+          .thenAnswer((invocation) async => Response('anything', 200));
 
-    await sut.request(
-      url: url,
-      method: HttpMethods.post,
-      headers: {
-        'custom-header': 'custom-value',
-      },
-    );
+      await sut.request(
+        url: url,
+        method: HttpMethods.post,
+        headers: {
+          'custom-header': 'custom-value',
+        },
+      );
 
-    verify(() => client.post(
-          uri,
-          headers: {
-            'content-type': 'application/json',
-            'accept': 'application/json',
-            'custom-header': 'custom-value',
-          },
-        ));
+      verify(() => client.post(
+            uri,
+            headers: {
+              'content-type': 'application/json',
+              'accept': 'application/json',
+              'custom-header': 'custom-value',
+            },
+          ));
+    });
   });
 }
